b8725f3b2ad4a032a85b705ff56ae5e647c804bf
René Kooi <rene@kooi.me>
2015-11-27 14:39:00 +0100

    fix overriding styles, add size="" prop to set width/height

--- src/icon.js
+++ src/icon.js
@@ -1,20 +1,19 @@
 var element = require('magic-virtual-element')
 
 exports.render = function render (component) {
+  var props = component.props
+  var size = 'size' in props ? props.size : 24
   var attrs = {
     xmlns: 'http://www.w3.org/2000/svg',
     viewBox: '0 0 24 24',
-    style: {
-      width: 24,
-      height: 24
-    }
+    style: 'width: ' + size + 'px; height: ' + size + 'px;'
   }
-  Object.keys(component.props).forEach(function (key) {
+  Object.keys(props).forEach(function (key) {
     if (key === 'style') {
-      assign(attrs.style, component.props.style)
+      attrs.style += ' ' + props.style
     } else if (key !== 'children') {
-      attrs[key] = component.props[key]
+      attrs[key] = props[key]
     }
   })
-  return element('svg', attrs, component.props.children)
+  return element('svg', attrs, props.children)
 }
