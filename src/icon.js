6fc594703b6079f058ab43ee0c96717e6395c006
René Kooi <rene@kooi.me>
2015-11-28 13:32:23 +0100

    allow any unit to be passed to size="" by using to-style

--- src/icon.js
+++ src/icon.js
@@ -1,19 +1,26 @@
 var element = require('magic-virtual-element')
+var toStyleString = require('to-style').string
 
 exports.render = function render (component) {
   var props = component.props
   var size = 'size' in props ? props.size : 24
+
+  var style = {
+    width: size,
+    height: size
+  }
+  if (props.color) {
+    style.fill = props.color
+  }
+
   var attrs = {
     xmlns: 'http://www.w3.org/2000/svg',
     viewBox: '0 0 24 24',
-    style: 'width: ' + size + 'px; height: ' + size + 'px;'
-  }
-  if (props.color) {
-    attrs.style += ' fill: ' + props.color + ';'
+    style: toStyleString(style)
   }
   Object.keys(props).forEach(function (key) {
     if (key === 'style') {
-      attrs.style += ' ' + props.style
+      attrs.style += '; ' + props.style
     } else if (key !== 'children') {
       attrs[key] = props[key]
     }
